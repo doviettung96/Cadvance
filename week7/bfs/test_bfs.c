78a146834af3186c4d9bb267de050bc1426dda28
doviettung96 <tunga9td@gmail.com>
2016-11-01 10:34:40 +0700

    bfs

---
+++ week7/bfs/test_bfs.c
@@ -0,0 +1,16 @@
+#include "graph_rb.h"
+
+void main()
+{
+	Graph g = createGraph();
+	addEdge(g, 0, 1);
+	addEdge(g, 1, 2);
+	addEdge(g, 1, 3);
+	addEdge(g, 2, 3);
+	addEdge(g, 2, 4);
+	addEdge(g, 4, 5);
+	printf("\nBFS: start from node 1 to 5: ");
+	BFS(g, 1, 4, printVertex);
+	printf("\nBFS: start from node 1 to all: ");
+	BFS(g, 1, -1, printVertex);
+}
